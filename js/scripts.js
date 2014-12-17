a41d74483b1486500a244d5ee26d8da875faedb1
Rajiv <rajiv.ranjan@above-inc.com>
2014-12-17 22:15:13 +0530

    Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-function sum(){
+var arrayData = [];
+function init(){
+
+	for(var i=1; i <= 10; i++){
+		var obj = {};
+		obj.id = i.toString();
+		obj.name = "ABC " + i;
+		obj.address = "Address " + i;
+		arrayData.push(obj);
+	}
+	displayData();
+}
+
+function displayData(){
+	var str = "<tr><td style='width:250px'>Name</td><td style='width:250px'>Address</td><td>Action</td></tr>"
+	for(var i=0; i < arrayData.length; i++){
+		str += "<tr><td style='width:250px'>"+ arrayData[i].name +"</td><td>"+ arrayData[i].address +"</td><td><input type='button' onclick='removeRecord(this.name)' name='"+ arrayData[i].id +"'' value='delete'></td></tr>"
+	}
+
+	document.getElementById("table1").innerHTML = str;
+}
+
+function removeRecord(id){
+	if(confirm('Are you sure to delete this record?')){
+		for(var i=0; i <= arrayData.length; i++){
+			if(arrayData[i].id == id){
+				arrayData.splice(i, 1);
+				break;
+			}
+		}
+		displayData();
+	}
 	
 }
