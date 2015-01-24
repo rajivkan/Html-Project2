f1a5885f6c56ee2f6d9aeeedd0f4ec2038fb8af0
Rajiv <rajiv.ranjan@above-inc.com>
2015-01-24 20:47:08 +0530

    angular

--- js/app.js
+++ js/app.js
@@ -7,28 +7,54 @@ htmlApp.config(['$routeProvider', '$controllerProvider',
                 controller: 'homeViewCtrl'
             })
             .when('/about', {
-                templateUrl: 'template/about.html',
+                template: '<div>About view</div>',
                 controller: 'aboutViewCtrl'
             })
             .when('/contact', {
                 templateUrl: 'template/contact.html',
                 controller: 'contactViewCtrl'
             })
+            .when('/detail/:personId', {
+                templateUrl: 'template/detail.html',
+                controller: 'detailViewCtrl'
+            })
            }
     ]);
 
-
+var arrayData = [];
 htmlApp.controller("homeViewCtrl", ["$scope", "$http", function($scope, $http){
 	$scope.message = "Home View Comtroller"
-	$scope.persons = [{"name": "ABc 1",
-				"address": "Address 1"},
-				{"name": "Abc 2", 
-				"address": "Address 2"},
-				{"name": "Abc 3", 
-				"address": "Address 3"},
-				{"name": "Abc 4", 
-				"address": "Address 4"}
-	];
+	// $scope.persons = [{"name": "ABc 1",
+	// 			"address": "Address 1"},
+	// 			{"name": "Abc 2", 
+	// 			"address": "Address 2"},
+	// 			{"name": "Abc 3", 
+	// 			"address": "Address 3"},
+	// 			{"name": "Abc 4", 
+	// 			"address": "Address 4"}
+	// ];
+
+	$http.get("data/data.json?1").success(function(result){
+		$scope.persons = result.persons;
+		arrayData = result.persons;
+	}).error(function(error){
+		alert("error");
+	});
+	$scope.displayView = function(){
+		alert("call");
+	}
+
+}]);
+
+htmlApp.controller("detailViewCtrl", ["$scope", '$routeParams', function($scope, $routeParams){
+	for(var i=0; i < arrayData.length; i++){
+		if(arrayData[i].id == $routeParams.personId){
+			$scope.name = arrayData[i].name;
+			$scope.address = arrayData[i].address;
+			$scope.courses = arrayData[i].course;
+			break;
+		}
+	}
 }]);
 
 htmlApp.controller("aboutViewCtrl", ["$scope", function($scope){
@@ -38,3 +64,16 @@ htmlApp.controller("aboutViewCtrl", ["$scope", function($scope){
 htmlApp.controller("contactViewCtrl", ["$scope", function($scope){
 	$scope.message = ""
 }]);
+
+htmlApp.directive("login", function(){
+	return{
+		restrict: 'C',
+		templateUrl: 'template/login.html',
+		controller: "loginController"
+	}
+});
+
+htmlApp.controller("loginController", ["$scope", function($scope){
+	$scope.username = "test";
+	$scope.password = "password";
+}])
